27a2093db0bc6a941e55292629c88a3b88daa643
Tim Schendekehl <tim.dlang@t-online.de>
2024-07-13 15:24:59 +0200

    Test wctype

--- test.d
+++ test.d
@@ -1,11 +1,34 @@
+import core.stdc.wctype;
 import std.stdio;
 
-extern(C++) int add(int a, int b);
+extern(C++) void printWcType();
 
 void main()
 {
-    int c = add(1, 2);
-    writeln("test ", c);
-    stdout.flush();
-    assert(c == 3);
+    writeln("C++:");
+    printWcType();
+
+    writeln("D:");
+    writeln("wint_t.sizeof ", wint_t.sizeof);
+    writeln("wctype_t.sizeof ", wctype_t.sizeof);
+    writeln("wctrans_t.sizeof ", wctrans_t.sizeof);
+    writeln(iswalpha('A'));
+    writeln(iswalpha('0'));
+    writeln(wctype("alpha"));
+    writeln(wctrans("tolower"));
+    writeln(iswctype('A', wctype("alpha")));
+    writeln(iswctype('0', wctype("alpha")));
+    writeln(towctrans('A', wctrans("tolower")));
+    writeln(towctrans('0', wctrans("tolower")));
+
+    assert(iswalpha('A'));
+    assert(!iswalpha('0'));
+    wctype_t alpha = wctype("alpha");
+    assert(alpha);
+    wctrans_t tolower = wctrans("tolower");
+    assert(tolower);
+    assert(iswctype('A', alpha));
+    assert(!iswctype('0', alpha));
+    assert(towctrans('A', tolower) == 'a');
+    assert(towctrans('0', tolower) == '0');
 }
--- testcpp.cpp
+++ testcpp.cpp
@@ -1,4 +1,17 @@
-int add(int a, int b)
+#include <stdio.h>
+#include <wctype.h>
+
+void printWcType()
 {
-    return a + b;
+    printf("wint_t.sizeof %zu\n", sizeof(wint_t));
+    printf("wctype_t.sizeof %zu\n", sizeof(wctype_t));
+    printf("wctrans_t.sizeof %zu\n", sizeof(wctrans_t));
+    printf("%d\n", iswalpha('A'));
+    printf("%d\n", iswalpha('0'));
+    printf("%lld\n", (long long)wctype("alpha"));
+    printf("%lld\n", (long long)wctrans("tolower"));
+    printf("%d\n", iswctype('A', wctype("alpha")));
+    printf("%d\n", iswctype('0', wctype("alpha")));
+    printf("%c\n", (char)towctrans('A', wctrans("tolower")));
+    printf("%c\n", (char)towctrans('0', wctrans("tolower")));
 }
